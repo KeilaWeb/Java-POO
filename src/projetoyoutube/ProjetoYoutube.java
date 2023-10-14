35b3bde713321014038599d3c1a9a9b6df7d1ba6
Keila D. Barreto <91154547+KeilaWeb@users.noreply.github.com>
2023-10-14 12:17:08 -0300

    finalizado

--- src/projetoyoutube/ProjetoYoutube.java
+++ src/projetoyoutube/ProjetoYoutube.java
@@ -11,8 +11,20 @@ public class ProjetoYoutube {
         g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
         g[1] = new Gafanhoto("Creuza", 18, "F", "créu");
 
+
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[0].avaliar(87.0f);
+        System.out.println(vis[0].toString());
+
+
         //System.out.println(v[0].toString());
-        System.out.println(g[0].toString());
+        //System.out.println(g[0].toString());
 
 
     }
